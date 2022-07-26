54a9808eee94600c6976de879046cb45fa6829d3
deepakdecrypt <deepak.decrypt@gmail.com>
2022-07-26 17:10:38 +0530

    Cron JOb update 10 - Deepak-cron

--- app/controllers/order/controllers.js
+++ app/controllers/order/controllers.js
@@ -112,7 +112,7 @@ class OrderController {
             } else {
               await Order.updateOne(
                 { _id: req.body.orderId },
-                { $set: { quantity_sold: req.body.qty_sold, hash: req.body.hash, hashStatus: req.body.hashStatus } },
+                { $set: { quantity_sold: req.body.qty_sold }, hashStatus: 1, contractAddress: contractAddress },
                 (err) => {
                   if (err) throw error;
                 }
