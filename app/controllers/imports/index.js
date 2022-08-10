aafcec7ecafdf90ef4f39ad9d0c2840747dd4705
deepakdecrypt <deepak.decrypt@gmail.com>
2022-08-10 13:08:36 +0530

    import NFT Apis update 2 - Deepak

--- app/controllers/imports/index.js
+++ app/controllers/imports/index.js
@@ -3,6 +3,7 @@ const ImportedController = require("./controllers");
 const importedController = new ImportedController();
 const nftMiddleware = require('./../helpers/middleware');
 
+router.post("/getMyImportedCollection", nftMiddleware.verifyAdminToken, importedController.getMyImportedCollection );
 router.post("/getImportedCollection", nftMiddleware.verifyAdminToken, importedController.getImportedCollection );
 router.post("/checkStatus", nftMiddleware.verifyAdminToken, importedController.checkStatus );
 router.post("/importedCollectionNFTs", nftMiddleware.verifyAdminToken, importedController.importedCollectionNFTs );
